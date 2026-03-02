100fff162088a55b53f2dbc1ca38d887a17cd708
tobias <tobiaskessels@cydefex.de>
2026-03-02 13:54:23 +0100

    build(scripts): optimize build flags for smaller binary

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ echo "Packing game data..."
 go run cmd/packer/main.go
 
 echo "Building binary..."
-go build -o ctf-tool main.go
+go build -trimpath -ldflags="-s -w" -o ctf-tool main.go
 
 echo "Done! Run ./ctf-tool to play."
